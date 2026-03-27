d72c3b881bc39fd81acedb867b744599bb095431
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-03-27 16:29:36 +0100

    test git ?

--- src/test/java/com/example/task/controller/TaskControllerIntegrationTests.java
+++ src/test/java/com/example/task/controller/TaskControllerIntegrationTests.java
@@ -1,6 +1,7 @@
 package com.example.task.controller;
 
 import com.example.task.service.TaskService;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,4 +15,13 @@ public class TaskControllerIntegrationTests {
 
     @Autowired
     private TaskService taskService; // real TaskService object automatically injected in TaskController instance
+
+
+
+
+
+
+
+
+
 }
--- src/test/java/com/example/task/controller/TaskControllerUnitTests.java
+++ src/test/java/com/example/task/controller/TaskControllerUnitTests.java
@@ -1,11 +1,14 @@
 package com.example.task.controller;
 
+
 import com.example.task.service.TaskService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -25,4 +28,6 @@ public class TaskControllerUnitTests {
                 .andExpect(status().isOk())
                 .andExpect(content().string("Welcome to the Task Manager API!"));
     }
+
+
 }
