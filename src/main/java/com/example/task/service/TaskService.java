3d209b9970c4688eb5ee625f43566bd0fd2ace53
Antoine Dru <antoine.dru@le-campus-numerique.fr>
2026-03-27 16:25:06 +0100

    Ajout des méthodes

--- src/main/java/com/example/task/service/TaskService.java
+++ src/main/java/com/example/task/service/TaskService.java
@@ -2,8 +2,42 @@ package com.example.task.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class TaskService {
-    /// TO BE IMPLEMENTED
-    /// Handle and manage a list of Task objects
+
+    private List<Task> tasks = new ArrayList<>();
+
+   public Task addTask(String description) {
+       Task task = new Task(description);
+       tasks.add(task);
+       return task;
+
+   }
+
+   public boolean removeTask(int id){
+       for (Task t : tasks){
+           if (t.getId() == id){
+               tasks.remove(t);
+               return true;
+           }
+       }
+       return false;
+   }
+
+   public List<Task> getTasks(){
+       return tasks;
+   }
+
+   public boolean completeTask(int id){
+       for (Task t : tasks){
+           if (t.getId() == id) {
+               t.setStatus("Terminé");
+               return true;
+           }
+       }
+       return false;
+   }
 }
